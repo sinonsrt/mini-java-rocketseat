1c402190f325234e37098db1ac44638eaea09cee
Lucas Marchiori <sinonsrt@hotmail.com>
2024-08-28 22:16:27 -0300

    feat: start/end date validation

--- src/main/java/br/com/lucasmarchiori/todolist/task/TaskController.java
+++ src/main/java/br/com/lucasmarchiori/todolist/task/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -24,6 +25,16 @@ public class TaskController {
         var userId = request.getAttribute("userId");
 
         taskModel.setUserId((UUID) userId);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start/end date must be greater than the current date");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date must be less than the end date");
+        }
 
         TaskModel createdTask = this.taskRepository.save(taskModel);
 
