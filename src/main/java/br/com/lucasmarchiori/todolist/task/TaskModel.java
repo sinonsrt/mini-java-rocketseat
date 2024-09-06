2d7e74589b783313a08abf107e659db77d948a41
Lucas Marchiori <sinonsrt@hotmail.com>
2024-09-05 22:04:09 -0300

    feat: http message not readable treatment handler

--- src/main/java/br/com/lucasmarchiori/todolist/task/TaskModel.java
+++ src/main/java/br/com/lucasmarchiori/todolist/task/TaskModel.java
@@ -30,5 +30,11 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception {
+        if(this.title.length() > 50) {
+            throw new Exception("Title exceed the 50 chars max length");
+        }
 
+        this.title = title;
+    }
 }
